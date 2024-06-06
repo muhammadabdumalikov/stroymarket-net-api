e2ace6e489d3ea98b1e87539c9ff06f14eee27b6
muhammadabdumalikov <burakali424@gmail.com>
2024-06-06 10:49:26 +0500

    Mitti nima qilyapsan???

--- Program.cs
+++ Program.cs
@@ -1,10 +1,21 @@
 using stroymarket_net_api.Entities;
 
+const string GetProductEndpoint = "GetProductEnpoint";
+
 List<Product> products = new()
 {
-  new Product()
+  new()
   {
     Id = 1,
+    NameRu = "Ruscha",
+    NameUz = "Uzbekcha",
+    Price = 19.11M,
+    ImageUri = "string",
+    CreatedAt = new DateTime(2024, 05, 10)
+  },
+  new()
+  {
+    Id = 2,
     NameRu = "Ruscha",
     NameUz = "Uzbekcha",
     Price = 19.11M,
@@ -16,6 +27,27 @@ List<Product> products = new()
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/products", () => products);
+
+app.MapGet("/products/{id}", (int id) =>
+{
+  Product? product = products.Find(game => game.Id == id);
+
+  if (product == null)
+  {
+    return Results.NotFound();
+  }
+
+  return Results.Ok(product);
+}
+).WithName(GetProductEndpoint);
+
+app.MapPost("/products", (Product product) =>
+{
+  product.Id = products.Max(product => product.Id) + 1;
+  products.Add(product);
+
+  return Results.CreatedAtRoute(GetProductEndpoint, new { id = product.Id }, product);
+});
 
 app.Run();
